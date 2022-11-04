ad19d0a6007cccecc168be9db3ad9dbf9af56975
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-11-04 13:36:31 +0900

    format code

--- server/app.js
+++ server/app.js
@@ -4,13 +4,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT // 5000;
 
 
 // middlewares 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
